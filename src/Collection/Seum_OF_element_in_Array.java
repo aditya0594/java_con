96edcff7f5194d6980893e4a54819648c7826fd2
Aditya Pawar <adityapawar180@gmail.com>
2023-08-18 01:02:52 +0530

    Array collection 18-08-2023

---
+++ src/Collection/Seum_OF_element_in_Array.java
@@ -0,0 +1,22 @@
+package Collection;
+
+public class Seum_OF_element_in_Array {
+    public static void main(String[] args) {
+//        int a[] ={2,2,2,2};
+//        int sum = 0;
+//        for(int i =0; i<=a.length-1; i++){
+//            sum = sum+a[i];
+//        }
+//        System.out.println(sum);
+
+
+    int a[] = {5,2,3,6,7,9,10};
+    int max = a[0];
+    for( int i=1; i<a.length;i++){
+        if(a[i]<max){
+            max=a[i];
+        }
+    }
+    System.out.println(max);
+    }
+}
