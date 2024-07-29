571609708a608aa9dff961eab1b6d42648c94a3e
aditya0594 <adityapawar0594@gmail.com>
2024-07-29 20:50:14 +0530

    updated added 15-06-2024 notes added

--- src/main/java/SeleniumTopic/Waits.java
+++ src/main/java/SeleniumTopic/Waits.java
@@ -39,10 +39,25 @@ public class Waits {
 }
 
 /**
-  FluentWait in Selenium is a type of wait that allows you to specify the maximum amount of time to wait for a condition, as well as the frequency with which to check the condition. It also allows you to specify which types of exceptions to ignore while waiting.
+  IMPLICIT WAIT :
+ An implicit wait is a global setting that tells the WebDriver to wait for a certain
+ amount of time before throwing a NoSuchElementException if an element is not immediately available.
+ Scope: It applies to all elements in the WebDriver instance.
+
+ EXPLICIT WAIT :
+ An explicit wait allows you to define a specific condition and wait for a particular element or condition
+ to be true before proceeding.
+ It is applied only to the specific element or condition it is defined for.
+ Uses WebDriver’s WebDriverWait class in combination with ExpectedConditions to wait for a condition to be met.
+
+ FLUENTWAIT :
+
+ FluentWait in Selenium is a type of wait that allows you to specify the maximum amount of time to wait for a condition,
+ as well as the frequency with which to check the condition.
+ It also allows you to specify which types of exceptions to ignore while waiting.
 
  When to Use FluentWait
-  FluentWait is useful when you need to:
+ FluentWait is useful when you need to:
 
  Wait for a condition with a custom time interval: If you want to check the condition at specific intervals instead of continuously, FluentWait allows you to set this interval.
   Handle specific exceptions: You can specify which exceptions to ignore during the wait period. This is helpful when certain exceptions are expected during the wait (e.g., NoSuchElementException).
